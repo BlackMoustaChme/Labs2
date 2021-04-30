51174c43f16af3be4b63e607ff81c444be2f0bed
BlackMoustaChe <chme@live.ru>
2021-04-30 08:53:52 +0700

    L4 1.0

--- 4/main.cpp
+++ 4/main.cpp
@@ -15,7 +15,7 @@ struct  Bunny {
 };
 
 void Bunny::out() {
-    cout << "Weight: " <<weight <<endl<< " " << "Age: " << age<<endl<< "Name: " << name <<endl<< "Hungry?: " << hunger <<endl;
+    cout << "Name: " << name<<endl<< " " << "Age: " << age<<endl<<  "Weight: " <<weight <<endl<< "Hungry?: " << hunger <<endl;
 }
 
 struct MyQueue
@@ -71,11 +71,12 @@ bool MyQueue::Pop(char*& data, int& n)
     Node* temp = First->next;
     //data = First->data;
     n=First->n;
+    data = new char[n];
     for(int i=0;i<n;i++)
     {
-        data[i]=First->data[i];
+        data[i]=(First->data)[i];
     }
-    delete First->data;
+    delete[] (First->data);
     delete First;
     First = temp;
     Count--;
@@ -93,18 +94,141 @@ void MyQueue::Info()
 }
 void GetDeSeria(char* , int , Bunny& );
 void GetSeria(char*& , int &, Bunny );
-int main()
+void PrintBinaryFile (char* , int );
+bool InputBinaryFile(MyQueue &);
+void Add (MyQueue& , Bunny& , char*&,int& );
+void Pull(MyQueue& , Bunny&, char*&,int&  );
+void Clearance(MyQueue& , Bunny&, char*&,int&  );
+void Menu();
+/*int main()
 {
-    Bunny d={"Bun",5.6,3,0};
+    MyQueue Q;
+    /*Bunny d={"Zayats",2.8,3,false};
     Bunny d1;
     d.out();
     int n;
     char* data;
     GetSeria(data,n,d);
-    GetDeSeria(data,n,d1);
-    d1.out();
+    PrintBinaryFile (data, n);
+    Bunny d2={"Volk",5.6,9,false};
+    GetSeria(data,n,d2);
+    PrintBinaryFile (data, n);
+    Bunny d3={"Gluttony",7.5,3,true};
+    GetSeria(data,n,d3);
+    PrintBinaryFile (data, n);
+    Bunny d4={"Mai",4.2,6,false};
+    GetSeria(data,n,d4);
+    PrintBinaryFile (data, n);
+    //GetDeSeria(data,n,d1);
+    //d1.out();
     delete[] data;
+    cout<<endl<<"   - - - - - - - - - -    "<<endl;
+    InputBinaryFile();
     return 0;
+}*/
+int main()
+{
+    Menu();
+    return 0;
+
+}
+
+void Menu()
+{
+    MyQueue Q;
+    Bunny A;
+    int n;
+    char* data;
+    InputBinaryFile(Q);
+    int key;
+
+    do {
+        cout<<endl;
+        cout << "1 - Add Bunny to the queue" << endl;
+        cout << "2 - Pull out Bunny" << endl;
+        cout << "3 - Clear the queue" << endl;
+        cout << "4 - Info on queue" << endl;
+        cout << "0 - Exit" << endl;
+
+        cout << "Choose what to do and enter the key " << endl;
+        cin >> key;
+
+        switch (key) {
+            case 1:
+                Add(Q,A,data,n);
+                break;
+            case 2:
+                Pull(Q,A,data,n);
+                break;
+            case 3:
+                Clearance(Q,A,data,n);
+                break;
+            case 4:
+                Q.Info();
+                break;
+            case 0:
+                cout << "Bye:)" << endl;
+                return;
+            default:
+                cout << "--->Error, try again... " << endl;
+        }
+    } while (key);
+    while(Q.Count)
+    {
+        Q.Pop(data, n);
+        delete[] data;
+    }
+}
+void Add (MyQueue& Q, Bunny& A, char*& data,int& n)
+{
+    cout << "Enter information about bunny:" << endl;
+    cout << "Name = "; cin >> A.name;
+    cout << "Weight = "; cin >> A.weight;
+    cout << "Age = "; cin >> A.age;
+    cout << "Hungry? - "; cin >> A.hunger;
+    GetSeria(data, n, A);
+    Q.Push(data, n);
+}
+void Pull(MyQueue& Q, Bunny& A,char*& data,int& n)
+{
+    Bunny Ax;
+    bool m = false;
+    cout << "Enter information about bunny:" << endl;
+    cout << "Name = "; cin >> Ax.name;
+    cout << "Weight = "; cin >> Ax.weight;
+    cout << "Age = "; cin >> Ax.age;
+    cout << "Hungry? - "; cin >> Ax.hunger;
+    int l = Q.Count;
+    for(int i = 0; i < l; i++)
+    {
+        Q.Pop(data, n);
+        GetDeSeria(data, n, A);
+        delete[] data;
+        if(A.name != Ax.name || A.weight != Ax.weight || A.age != Ax.age || A.hunger != Ax.hunger)
+        {
+            GetSeria(data, n, A);
+            Q.Push(data, n);
+        }
+        else
+        {
+            m = true;
+            break;
+        }
+    }
+    if(!m) cout << "Product not found" << endl;
+}
+void Clearance(MyQueue& Q, Bunny& A, char*& data,int& n)
+{
+    while(Q.Count)
+    {
+        Q.Pop(data, n);
+        GetDeSeria(data, n, A);
+        delete[] data;
+        cout << "Name : " << A.name<< endl;
+        cout << "Weight = " << A.weight<< endl;
+        cout << "Age = " << A.age<< endl;
+        cout << "Hungry? - " << A.hunger<< endl << endl;
+    }
 }
 void GetSeria(char*& data, int &n, Bunny A)
 {
@@ -142,28 +266,39 @@ void GetDeSeria(char* data, int n, Bunny& A)
     A.age = *reinterpret_cast<int*> (data + n1_size+n2+n3);
     A.hunger = *reinterpret_cast<bool*> (data + n1_size+n2+n3+n4);
 }
-/*bool InputBinaryFile(MyQueue &Q)
+void PrintBinaryFile (char* data, int n)
 {
-  fstream f_in("out.dat", ios::in | ios::binary);
-  if(!f_in)
-  {
-    return false;
-  }
-   A;
-  int n = 0;
-  while(!f_in.eof())
-  {
-    if(f_in.read((char*) &n, sizeof(int)))
+    fstream f_out ("out.dat", ios::app | ios::binary);
+    f_out.write ((char*)&n,sizeof(int));
+    f_out.write (data,n);
+    f_out.close();
+}
+bool InputBinaryFile(MyQueue &Q)
+{
+    fstream f_in("out.dat", ios::in | ios::binary);
+    if(!f_in)
     {
-      char* data = new char[n];
-      f_in.read(data, n);
-      GetDeSeria(data, n, A);
-      delete[] data;
-      GetSeria(data, n, A);
-      Q.Push(data, n);
+        return false;
     }
-  }
-  f_in.close();
-  return true;
-}*/
+    Bunny A;
+    int n;
+    while(!f_in.eof())
+    {
+        if(f_in.read((char*) &n, sizeof(int)))
+        {
+            char* data = new char[n];
+            f_in.read(data, n);
+            GetDeSeria(data, n, A);
+            cout << "Name : " << A.name<< endl;
+            cout << "Weight = " << A.weight<< endl;
+            cout << "Age = " << A.age<< endl;
+            cout << "Hungry? - " << A.hunger<< endl << endl;
+            GetSeria(data, n, A);
+            Q.Push(data, n);
+            delete[] data;
+        }
+    }
+    f_in.close();
+    return true;
+}
 
